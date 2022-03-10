db9a2a45e98002b2a263c303b6c6fe18019d95e0
justamoon <luanadegrande9@gmail.com>
2022-03-09 22:02:55 -0300

    Sexto

---
+++ Desafio06.py
@@ -0,0 +1,12 @@
+#Faça um programa que leia nome e média de um aluno, guardando também a situação em um dicionário. No final, mostre o conteúdo da 
+#estrutura na tela.
+
+nome = input('Nome do aluno:')
+media = float(input('Média do aluno:'))
+if media >= 5 and media <= 10:
+    situacao = 'Aprovado'
+if media <5:
+    situacao = 'Reprovado'
+resultados_sala = {'Nome': nome.capitalize(), 'Média': media, 'Situação': situacao}  
+
+print(resultados_sala, type(resultados_sala))

b71e8c584cf71afee2f57b335948297c32a70931
justamoon <luanadegrande9@gmail.com>
2022-08-23 15:19:48 -0300

    Correção

--- Desafio10.py
+++ Desafio10.py
@@ -6,14 +6,16 @@ def fatorial(n, show):
         show = bool(int(show))
     except:
         show == False #aqui, considerei que ausência de resposta ou inputs inválidos serão lidos como falso, p garantir opção
-    calc = int(n)
     fat = 1
-    while calc > 0:
-        fat = fat*calc
+    for f in range(n, 0, -1):
         if show == True:
-            print('{} * {}'.format(fat,calc))
-        calc -= 1
+            print(f, end = '')
+            if n > 1:
+                print(' * ', end='')
+            else:
+                print(' = ', end = '')
+        fat *= f
     return n, fat
 
-f = fatorial(input('Digite um valor: '), input('Deseja visualizar o processo? \n 0 - não \n 1 - sim \n'))
+f = fatorial(int(input('Digite um valor: ')), input('Deseja visualizar o processo? \n 0 - não \n 1 - sim \n'))
 print('O fatorial de {} é {}'.format(f[0], f[1]))

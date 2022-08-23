d1ba6c510332149ca83974a254723cc385ac2bde
justamoon <luanadegrande9@gmail.com>
2022-08-23 13:50:58 -0300

    09 feito

--- Desafio09.py
+++ Desafio09.py
@@ -1,13 +1,20 @@
 #Crie um programa que tenha a função leiaInt(), que vai funcionar de forma semelhante 'a função input() do Python,
 #só que fazendo a validação para aceitar apenas um valor numérico.
 
-def leiaInt(nume):
-    if nume is not int(nume):
-        print('Digite um valor numérico que seja inteiro')
-        return None
-    else:
-        return nume
+def leiaInt(msg):
+    ok = False
+    valor = 0
+    while True:
+        n = str(input(msg))
+        if n.isnumeric():
+            valor = int(n)
+            ok = True
+        else:
+            print('\033[0;31mERRO! Digite um número inteiro válido. \033[m')
+        if ok:
+            break
+    return valor
+#Programa Principal
 
-leiaInt(input('Digite um número: '))
-
-#TODO: não finalizado
+n = leiaInt('Digite um número: ')
+print('Você acabou de digitar o número {}'.format(n))
